03b30b950ba8c9de1ca70c8a8a850384096f8897
ibrahimbanat <Ibrahim.banat@altibbi.com>
2021-11-13 15:41:40 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  };
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  OnNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
